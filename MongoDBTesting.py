1f16cdce159cc8d107e5720abd7d0d61deba3ae2
MarkStoneLin <20142950@s.hlju.edu.cn>
2020-03-30 03:37:14 +0800

    Add MongoDB learning file

--- MongoDBTesting.py
+++ MongoDBTesting.py
@@ -1,40 +1,13 @@
-#!/usr/bin/env python
-
-from pymongo import Connection
-import time
-import datetime
-
-connection = Connection('115.220.10.112', 27017)
-db = connection['hawaii']
-
-#时间记录器
-def func_time(func):
-
-    def _wrapper(*args,**kwargs):
-
-        start = time.time()
-        func(*args,**kwargs)
-        print func.name,'run:',time.time()-start
-    return _wrapper
-
-@func_time
-def insert(num):
-
-    posts = db.userinfo
-
-    for x in range(num):
-
-        post = {"_id" : str(x),
-                "author": str(x)+"Mike",
-                "text": "My first blog post!",
-                "tags": ["mongodb", "python", "pymongo"],
-                "date": datetime.datetime.utcnow()}
-
-        posts.insert(post)
+import pymongo
 
 
-if name == "main":
+myclient = pymongo.MongoClient("mongodb://115.220.10.112:27017/")
+mydb = myclient["admin"]       #test-db是数据库名称
+mydb.authenticate('myUserAdmin','abc123')
 
-    #设定循环500万次
-    num = 5000000
-    insert(num)
+
+tdb = myclient.admin
+post = tdb.test
+print("操作ing")
+post.insert({'name':"李白", "age":"30", "skill":"Python"})
+print("操作完成")
---
+++ MongoDB_Learning.md
@@ -0,0 +1,38 @@
+# MongoDB 命令行基本操作
+
+
+
+1. 进入MongoDB
+
+   输入
+
+   ```
+   mongo 
+   ```
+
+   
+
+2. 进入数据库
+
+   ```
+   use DatabaseName
+   ```
+
+   
+
+3. 输入用户名和密码
+
+   ```
+   db.auth("myUserAdmin","abc123")
+   ```
+
+
+
+4. 查询数据
+
+   ```
+   db.test.find()
+   ```
+
+   
+
