08c3a2f8fd8cfc5a94144f5aa138e392ebf346de
MarkStoneLin <20142950@s.hlju.edu.cn>
2020-04-17 01:18:35 +0800

    Submit Testing modify in before

--- MongoDBTesting.py
+++ MongoDBTesting.py
@@ -9,10 +9,22 @@ mydb.authenticate('myUserAdmin','abc123')
 tdb = myclient.admin
 post = tdb.test         #test是集合，也就是表
 
-print("执行插入操作ing")
-post.insert({'name':"李白", "age":"30", "skill":"Python"})
-print("插入操作完成")
+#print("执行插入操作ing")
+#post.insert({'name':"李白", "age":"30", "skill":"Python"})
+#print("插入操作完成")
 
 print("执行查询操作ing")
-result = post.find_one({'name': '李白'})
-print(result)
+#result = post.find_one({'style': 'thai'},{'style': 1,'img_url': 1,'_id': 0})
+
+result1 = post.find_one({'style': 'china'},{'style': 1,'popular_menu': {"$slice":1},'_id': 0})
+result2 = post.find_one({'style': 'western'},{'style': 1,'popular_menu.img_url': 1,'_id': 0})
+result3 = post.find_one({'style': 'japan'},{'style': 1,'popular_menu.img_url': 1,'_id': 0})
+
+
+print(post.distinct('style'))
+
+print(result1,"\n",result2,"\n",result3)
+
+# for x in post.find({},{'style': 1,'popular_menu.img_url': 1,'_id': 0}):
+#     print(x)
+
